2b60114ebaeee8c1a64189a7ae67462af0338f08
shazim8480 <shazim_99@yahoo.com>
2021-03-26 02:23:00 +0600

    final deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import TeamDetails from "./Components/TeamDetails/TeamDetails";
 
